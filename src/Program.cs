9654b4a25de729964a97f61dc152bf5a1ddfd036
wasikuss <wasikuss@gmail.com>
2022-05-03 18:10:25 +0200

    Add project sources.

---
+++ src/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program {
+    public static int Main(string[] args) {
+        var rootPath = args.Length == 1 ? args[0] : Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+        new LangPatcher(rootPath).Run();
+        return 0;
+    }
+}
